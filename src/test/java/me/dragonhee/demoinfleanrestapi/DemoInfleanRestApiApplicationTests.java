15943ba0bf29fe5d1ffc7798c6945ba67d15ed1b
kimdragonhee <kim.dragon.hee@gmail.com>
2021-02-01 22:14:19 +0900

    first commit

---
+++ src/test/java/me/dragonhee/demoinfleanrestapi/DemoInfleanRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dragonhee.demoinfleanrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInfleanRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
